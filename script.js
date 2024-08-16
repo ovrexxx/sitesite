9518d5c7915e41bd0df1b3c445609e579ac4c8d4
ovrexxx <moneygotmoney69@gmail.com>
2024-08-16 07:32:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function generateRandomKey() {
-    let key = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (let i = 0; i < 10; i++) {
-        key += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-    return key;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    const keyElement = document.createElement('p');
-    keyElement.textContent = `Key: ${generateRandomKey()}`;
-    document.body.appendChild(keyElement);
-});
